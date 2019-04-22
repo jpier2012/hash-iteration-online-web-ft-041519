44235f212143f344e38041001bab625960999845
James Pier <jpier2012@gmail.com>
2019-04-22 14:45:26 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,7 +10,7 @@ def select_winner(passengers)
   # add the code snippet here!
   passenger_name = ""
   passengers.each { |suite, name|
-    if suite.to_s.ends_with?('a') && name.start_with?('A')
+    if suite.to_s.end_with?('a') && name.start_with?('A')
     passenger_name = name
     end
   }
