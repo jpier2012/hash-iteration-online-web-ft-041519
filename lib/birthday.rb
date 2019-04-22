c603f4d31bfdaa109e524a05a9dcc0b6da1ce1a1
James Pier <jpier2012@gmail.com>
2019-04-22 14:24:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,6 +7,6 @@
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each {|kid, age|
-    puts "Happy Birthday #{kid}! You are now #{age} years old!
+    puts "Happy Birthday #{kid}! You are now #{age} years old!"
   }
 end
