b327340ab10a211aa4cde25e4c24ed87c0a64edb
James Pier <jpier2012@gmail.com>
2019-04-22 14:28:58 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -7,12 +7,14 @@ birthday_kids = {
 def happy_birthday(birthday_kids)
   # add your code snippet here!
   birthday_kids.each {|kid, age|
-    if age <= 12
+    #if age <= 12
       puts "Happy Birthday #{kid}! You are now #{age} years old!"
-    else
-      puts "You are too old for this."
+    # else
+    #   puts "You are too old for this."
     end
   }
 end
 
 #happy_birthday(birthday_kids)
+
+
